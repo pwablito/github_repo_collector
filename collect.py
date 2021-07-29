d50662665c6117f8d1ab133f64cc784feb17a885
pwablito <pwablito@gmail.com>
2021-07-29 13:46:58 -0600

    Better arguments, progress, downloading, and search for users

--- collect.py
+++ collect.py
@@ -7,15 +7,22 @@ from repo_collector import RepoCollector
 
 def main():
     parser = argparse.ArgumentParser("Collect Github repositories by uer")
-    parser.add_argument("-u", "--user", required=True)
+    parser.add_argument("-u", "--users", required=True, nargs="+")
     parser.add_argument("-t", "--token", required=True)
+    parser.add_argument("-l", "--layers", required=False, type=int)
+    parser.add_argument("-v", "--verbose", action="store_true")
     args = parser.parse_args()
 
     collector = RepoCollector(args.token)
-
-    collector.fetch_connections(args.user)
+    for user in args.users:
+        collector.add_user(user)
+    if args.layers:
+        for _ in range(args.layers):
+            collector.fetch_connections()
+    if args.verbose:
+        print("Included users: {}".format(", ".join(collector.users)))
     collector.fetch_repos()
-    collector.download_repos()
+    collector.download_repos(progress=args.verbose)
 
 
 if __name__ == "__main__":
--- repo_collector.py
+++ repo_collector.py
@@ -1,26 +1,39 @@
 from github import Github
 import pygit2
 import os
+import shutil
 
 
 class RepoCollector:
-    def __init__(self, token):
+    def __init__(self, token, download_dir="downloads"):
+        self.download_dir = download_dir
         self.token = token
         self.g = Github(token)
         self.users = set()
         self.repos = []
 
-    def fetch_connections(self, username):
-        for user in self.g.get_user(username).get_following():
-            self.users.add(user.login)
-        for user in self.g.get_user(username).get_followers():
-            self.users.add(user.login)
+    def add_user(self, username):
+        self.users.add(username)
+
+    def fetch_connections(self):
+        users = self.users.copy()
+        for username in users:
+            for user in self.g.get_user(username).get_following():
+                self.users.add(user.login)
+            for user in self.g.get_user(username).get_followers():
+                self.users.add(user.login)
 
     def fetch_repos(self):
         for user in self.users:
             for repo in self.g.get_user(user).get_repos():
                 self.repos.append(repo)
 
-    def download_repos(self):
-        for repo in self.repos:
-            pygit2.clone_repository(repo.clone_url, os.path.join("downloads", repo.owner.login, repo.name))
+    def download_repos(self, progress=True):
+        for i in range(len(self.repos)):
+            if (progress):
+                print("Downloading repo {} of {}: {}/{}".format(i + 1, len(self.repos), self.repos[i].owner.login, self.repos[i].name))
+            repo = self.repos[i]
+            destination = os.path.join(self.download_dir, repo.owner.login, repo.name)
+            if os.path.exists(destination):
+                shutil.rmtree(destination)
+            pygit2.clone_repository(repo.clone_url, destination)
